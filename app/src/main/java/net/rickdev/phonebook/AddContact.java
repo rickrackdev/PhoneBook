23e1e5e692b23d8e3aa1a10d1e9ebfbb690b9b91
rick <alvarenga409@gmail.com>
2024-10-24 18:30:28 -0600

    added layout for the add contacts activity and added database connection to the java file

--- app/src/main/java/net/rickdev/phonebook/AddContact.java
+++ app/src/main/java/net/rickdev/phonebook/AddContact.java
@@ -8,12 +8,19 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import net.rickdev.phonebook.clases.SQLiteConnection;
+
 public class AddContact extends AppCompatActivity {
+
+    SQLiteConnection objConnection;
+    final String DATABASE_NAME = "contactsBook";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_add_contact);
+
+        objConnection = new SQLiteConnection(AddContact.this, DATABASE_NAME, null, 1);
     }
 }
