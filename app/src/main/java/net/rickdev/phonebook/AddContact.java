6332a067b39cb0bce9e36cee7a8b78ce09b481ba
rick <alvarenga409@gmail.com>
2024-10-24 23:24:19 -0600

    implemented feature to display contacts in the lvContacts listview, connecting the database to readOnly and update each time the main activity is opened

--- app/src/main/java/net/rickdev/phonebook/AddContact.java
+++ app/src/main/java/net/rickdev/phonebook/AddContact.java
@@ -1,6 +1,12 @@
 package net.rickdev.phonebook;
 
+import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,6 +21,9 @@ public class AddContact extends AppCompatActivity {
     SQLiteConnection objConnection;
     final String DATABASE_NAME = "contactsBook";
 
+    EditText edtName, edtNumber;
+    Button btnSave, btnBack;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,5 +31,43 @@ public class AddContact extends AppCompatActivity {
         setContentView(R.layout.activity_add_contact);
 
         objConnection = new SQLiteConnection(AddContact.this, DATABASE_NAME, null, 1);
+         edtName = findViewById(R.id.edtName);
+         edtNumber = findViewById(R.id.edtNumber);
+         btnSave = findViewById(R.id.btnSave);
+         btnBack = findViewById(R.id.btnBack);
+
+         btnSave.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View v) {
+                addContact();
+             }
+         });
+
+         btnBack.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View v) {
+                 goBack();
+             }
+         });
+    }
+
+
+    private void addContact(){
+        try {
+            SQLiteDatabase myDataBase = objConnection.getWritableDatabase();
+            String insert = "INSERT INTO contacts (name, phoneNumber) VALUES ('"+edtName.getText()+"','"+edtNumber.getText()+"')";
+            myDataBase.execSQL(insert);
+            myDataBase.close();
+            Toast.makeText(AddContact.this, "Contact saved succesfully", Toast.LENGTH_LONG).show();
+        } catch (Exception error) {
+            Toast.makeText(AddContact.this, "Error: " + error.getMessage(), Toast.LENGTH_LONG).show();
+        }
+    }
+
+
+    private void goBack(){
+        Intent goBack = new Intent(AddContact.this, MainActivity.class);
+        startActivity(goBack);
+        AddContact.this.finish();
     }
 }
--- app/src/main/java/net/rickdev/phonebook/MainActivity.java
+++ app/src/main/java/net/rickdev/phonebook/MainActivity.java
@@ -1,6 +1,14 @@
 package net.rickdev.phonebook;
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,9 +18,16 @@ import androidx.core.view.WindowInsetsCompat;
 
 import net.rickdev.phonebook.clases.SQLiteConnection;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
     SQLiteConnection objConnection;
     final String DATABASE_NAME = "contactsBook";
+
+    Button btnSearch, btnAdd;
+    ListView contactList;
+    ArrayList<String> list;
+    ArrayAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +35,45 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         objConnection = new SQLiteConnection(MainActivity.this, DATABASE_NAME, null, 1);
+        btnAdd = findViewById(R.id.btnAdd);
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent addContact = new Intent(MainActivity.this, AddContact.class);
+                startActivity(addContact);
+            }
+        });
+
+        contactList = findViewById(R.id.lvContacts);
+        list = fillList();
+        adapter = new ArrayAdapter(MainActivity.this, android.R.layout.simple_list_item_1, list);
+        contactList.setAdapter(adapter);
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        try {
+            list.clear();
+            list = fillList();
+            adapter = new ArrayAdapter(MainActivity.this, android.R.layout.simple_list_item_1, list);
+            contactList.setAdapter(adapter);
+        }catch (Exception error){
+            Toast.makeText(MainActivity.this, "Error: " + error.getMessage(), Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public ArrayList fillList(){
+        ArrayList<String> myList = new ArrayList<>();
+        SQLiteDatabase base = objConnection.getReadableDatabase();
+        String query = "SELECT id_contact, name, phoneNumber FROM contacts ORDER BY name ASC";
+        Cursor eachRegistry = base.rawQuery(query, null);
+        if (eachRegistry.moveToFirst()){
+            do{
+                myList.add(eachRegistry.getString(1).toString());
+            }while (eachRegistry.moveToNext());
+        }
+        return myList;
+    }
+
 }
